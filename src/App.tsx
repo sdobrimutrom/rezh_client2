503a9782cfa3b891da1beeac57da29e5c792e6d2
dclxxxvi <t.aleksandr888@mail.ru>
2022-10-23 01:58:28 +0500

    Конфигурация хранилища, добавление среды окружения

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Provider } from 'react-redux';
+
 import MainRouter from './router/MainRouter';
+import { store } from './store/store';
 
 function App() {
-    return <MainRouter />;
+    return (
+        <Provider store={store}>
+            <MainRouter />
+        </Provider>
+    );
 }
 
 export default App;
