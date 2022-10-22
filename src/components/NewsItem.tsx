f9c7c4e46e0f37dcf9bb6bdac5a3f5b52444eb3a
dclxxxvi <t.aleksandr888@mail.ru>
2022-10-22 21:19:48 +0500

    новость элемент

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,4 +1,11 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid, styled, Typography } from '@mui/material';
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%'
+});
 
 interface NewsItemProps {
     id: number;
@@ -10,8 +17,13 @@ interface NewsItemProps {
 
 export default function NewsItem({ id, title, createdAt, text, img }: NewsItemProps) {
     return (
-        <Paper>
-            <Grid>{title}</Grid>
-        </Paper>
+        <Grid container direction="row">
+            <Grid item>
+                <Img alt="альтернатива" />
+            </Grid>
+            <Grid item>
+                <Typography component="h5">{title}</Typography>
+            </Grid>
+        </Grid>
     );
 }
