310e5e002b453fd2d60c6f883c1a01006ca22c3e
dclxxxvi <t.aleksandr888@mail.ru>
2022-10-28 00:54:37 +0500

    валидация форм formik+yup, login form

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,22 +1,63 @@
+import { Close, Visibility, VisibilityOff } from '@mui/icons-material';
 import {
     Box,
+    Button,
     Card,
     FormControl,
     FormHelperText,
     Grid,
-    Input,
+    IconButton,
+    InputAdornment,
     InputLabel,
     Modal,
+    OutlinedInput,
+    TextField,
     Typography
 } from '@mui/material';
-import React from 'react';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
 
 interface LoginModalProps {
     open: boolean;
     handleClose: () => void;
 }
 
+const validationSchema = yup.object({
+    email: yup.string().email('Некорректный email').required('Введите email'),
+    password: yup.string().required('Введите пароль')
+});
+
 export default function LoginModal({ open, handleClose }: LoginModalProps) {
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: validationSchema,
+        validateOnBlur: false,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+
+    const navigateTo = (to: string) => {
+        handleClose();
+        navigate(to);
+    };
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleNotShowPassword = () => {
+        setShowPassword(false);
+    };
+
     return (
         <Modal
             open={open}
@@ -24,29 +65,104 @@ export default function LoginModal({ open, handleClose }: LoginModalProps) {
             style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
             aria-labelledby="parent-modal-title"
             aria-describedby="parent-modal-description">
-            <Card>
-                <Box padding={6} minWidth={320}>
-                    <Grid container direction={'column'} gap={4} justifyContent={'space-between'}>
-                        <Grid container direction={'column'} justifyContent={'space-between'}>
-                            <Typography variant="h4" fontWeight={700}>
-                                Авторизация
-                            </Typography>
-                            <Typography variant="subtitle1" fontWeight={600} color={'gray'}>
-                                Войдите в свой аккаунт
-                            </Typography>
+            <Card
+                sx={{
+                    boxShadow: 6,
+                    borderRadius: 3,
+                    border: 'none',
+                    width: 500
+                }}>
+                <Grid container paddingTop={2} justifyContent={'flex-end'}>
+                    <Button onClick={handleClose} style={{ backgroundColor: 'transparent' }}>
+                        <Close color="disabled" />
+                    </Button>
+                </Grid>
+                <Box padding={6} paddingTop={1}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <Grid
+                            container
+                            direction={'column'}
+                            gap={4}
+                            justifyContent={'space-between'}>
+                            <Grid container direction={'column'} justifyContent={'space-between'}>
+                                <Typography variant="h4" fontWeight={700}>
+                                    Авторизация
+                                </Typography>
+                                <Typography variant="subtitle1" fontWeight={600} color={'gray'}>
+                                    Войдите в свой аккаунт
+                                </Typography>
+                            </Grid>
+                            <Grid container direction={'column'} gap={3}>
+                                <TextField
+                                    id="email"
+                                    name="email"
+                                    label="Электронная почта"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.email && Boolean(formik.errors.email)}
+                                    helperText={formik.touched.email && formik.errors.email}
+                                />
+                                <Grid container direction={'column'} gap={1}>
+                                    <FormControl>
+                                        <InputLabel
+                                            error={
+                                                formik.touched.password &&
+                                                Boolean(formik.errors.password)
+                                            }
+                                            htmlFor="password">
+                                            Пароль
+                                        </InputLabel>
+                                        <OutlinedInput
+                                            id="password"
+                                            name="password"
+                                            label="Пароль"
+                                            type={showPassword ? 'text' : 'password'}
+                                            value={formik.values.password}
+                                            onChange={formik.handleChange}
+                                            error={
+                                                formik.touched.password &&
+                                                Boolean(formik.errors.password)
+                                            }
+                                            endAdornment={
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        aria-label="toggle password visibility"
+                                                        onClick={handleShowPassword}
+                                                        onMouseDown={handleNotShowPassword}>
+                                                        {showPassword ? (
+                                                            <Visibility />
+                                                        ) : (
+                                                            <VisibilityOff />
+                                                        )}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            }
+                                        />
+                                        <FormHelperText error id="password">
+                                            {formik.touched.password && formik.errors.password}
+                                        </FormHelperText>
+                                    </FormControl>
+                                    <Button
+                                        style={{
+                                            color: 'gray',
+                                            marginLeft: 'auto',
+                                            textTransform: 'none',
+                                            backgroundColor: 'transparent'
+                                        }}
+                                        onClick={() => navigateTo('/forget')}>
+                                        Забыли пароль?
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Grid container direction={'column'} gap={1} alignItems={'center'}>
+                                <Button variant={'contained'} type={'submit'}>
+                                    Авторизоваться
+                                </Button>
+                                <Typography variant="body2">или</Typography>
+                                <Button variant={'outlined'}>Зарегистрироваться</Button>
+                            </Grid>
                         </Grid>
-                        <Grid container direction={'column'} gap={3}>
-                            <FormControl>
-                                <InputLabel htmlFor="email">Электронная почта</InputLabel>
-                                <Input id="email" />
-                            </FormControl>
-                            <FormControl>
-                                <InputLabel htmlFor="password">Пароль</InputLabel>
-                                <Input id="password" />
-                            </FormControl>
-                        </Grid>
-                        <Grid container direction={'column'}></Grid>
-                    </Grid>
+                    </form>
                 </Box>
             </Card>
         </Modal>
