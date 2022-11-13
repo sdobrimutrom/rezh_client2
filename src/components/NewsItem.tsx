d7d7241189c461d58a19dc802b8ba8165e919328
SemenGladyshev <gladyshev15081508@gmail.com>
2022-11-13 17:24:22 +0500

    News Cards

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row';
 
 interface NewsItemProps {
     id: number;
@@ -8,49 +14,16 @@ interface NewsItemProps {
     img: string;
 }
 
-export function SortItem() {
-    const [sort, setSort] = React.useState('');
-
-    return (
-        <div></div>
-        // <Box sx={{ justifyContent: 'flex-end', width: 120 }}>
-        //     <FormControl fullWidth>
-        //         <InputLabel>Сортировать</InputLabel>
-        //         <Select value={sort} label="Сортировать" onChange={handleChange}>
-        //             <MenuItem>По дате</MenuItem>
-        //             <MenuItem>По заголовку</MenuItem>
-        //             <MenuItem>По номеру</MenuItem>
-        //         </Select>
-        //     </FormControl>
-        // </Box>
-    );
-}
-
 export default function NewsItem({ id, title, createdAt, text, img }: NewsItemProps) {
     return (
-        <div></div>
-        // <Box
-        //     sx={{
-        //         width: 500,
-        //         height: 300,
-        //         backgroundColor: 'primary dark',
-        //         border: '1px solid black',
-        //         marginBottom: '40px'
-        //     }}>
-        //     <Grid container direction="row">
-        //         <Grid item>
-        //             <Typography component="h5">{id}).</Typography>
-        //         </Grid>
-        //         <Grid item>
-        //             <Typography component="h5">{title}</Typography>
-        //         </Grid>
-        //         <Grid item>
-        //             <Img src={img} alt="альтернатива" />
-        //         </Grid>
-        //         <Grid item>
-        //             <Typography component="h4">{text}</Typography>
-        //         </Grid>
-        //     </Grid>
-        // </Box>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{text}</Card.Text>
+                <Button variant="primary">Читать далее</Button>
+            </Card.Body>
+        </Card>
     );
+
 }
