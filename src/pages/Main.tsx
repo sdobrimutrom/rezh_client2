9a7557bafab3cdc3e45c4d96a2dc9218df516e2f
SemenGladyshev <gladyshev15081508@gmail.com>
2022-11-23 20:25:11 +0500

    MainPage поправил новости

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,19 @@
+import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/Form';
 import image from '../Images/MainPage_1.png';
 import InputGroup from 'react-bootstrap/InputGroup';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import sliderImage_1 from "../Images/slider1.png"
-import sliderImage_2 from "../Images/slider2.png"
-import sliderImage_3 from "../Images/slider3.png"
+import sliderImage_1 from "../Images/slider1.png";
+import sliderImage_2 from "../Images/slider2.png";
+import sliderImage_3 from "../Images/slider3.png";
+import newsImage_1 from "../Images/News1.png";
+import newsImage_2 from "../Images/News2.png";
+import newsImage_3 from "../Images/News3.png";
 
 function FirstMainBlock() {
     return(
@@ -28,25 +31,94 @@ function FirstMainBlock() {
                                         Amet minim mollit non deserunt ullamco.
                                     </h1>
                                 </Col>
-                            </Row>
-                            <Row>
                                 <Col>
-                                    <InputGroup style = {{ "height": "70px", "width": "795px", marginLeft: "100px" }}>
-                                        <Form.Control
-                                            placeholder = "Введите текст.."
-                                            aria-label = "Введите текст.."
-                                            aria-describedby="basic-addon2"/>
-                                        <Button
-                                            variant="outline-secondary"
-                                            style = {{ "height": "70px", "width": "125px", marginLeft: "0px" }}>
-                                            Поиск
-                                        </Button>
-                                    </InputGroup>
+                                    <div className="list-group me-3" style = {{ "width": "355px", marginTop: "80px" }}>
+                                        <a href="#!"
+                                           className="list-group-item list-group-item-action list-group-item-secondary">Dapibus
+                                            ac facilisis in</a>
+                                        <a href="#!"
+                                           className="list-group-item list-group-item-action list-group-item-secondary">Dapibus
+                                            ac facilisis in</a>
+                                        <a href="#!"
+                                           className="list-group-item list-group-item-action list-group-item-secondary">Dapibus
+                                            ac facilisis in</a>
+                                    </div>
+
                                 </Col>
                             </Row>
                     </Container>
                 </Card.ImgOverlay>
             </Card>
+        </>
+    );
+}
+
+function NewsBlock() {
+    return(
+        <>
+            <Container className = "ms-2" fluid>
+                <Row>
+                    <Col>
+                        <h1 style={{ "width": "440px", "height": "56px" }}
+                            className="text-center ms-3 bg-secondary text-white">
+                            Фильтры</h1>
+                        <div className = "ms-3 mt-0" style={{ "width":"440px" }}>
+                            <Form.Select aria-label = "default">
+                                <option>Тип</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                            <Form.Select aria-label = "default">
+                                <option>Все сферы</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                            <Form.Select aria-label = "default">
+                                <option>Источник</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                            <Form.Select aria-label = "default">
+                                <option>Период публикации</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </div>
+
+                        <button type="button" className="btn btn-secondary text-center ms-3 w-600" style = {{ "width": "440px", "height": "77px" }}>Показать</button>
+                    </Col>
+                    <Col className = "me-3">
+                        <Card className="bg-dark text-white mb-3">
+                            <Card.Img src={newsImage_1} alt="Card image"/>
+                            <Card.ImgOverlay>
+                                <Card.Header>*Время* *Сфера*</Card.Header>
+                                <Card.Text className="align-text-top">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</Card.Text>
+                            </Card.ImgOverlay>
+                        </Card>
+                        <Card className="bg-dark text-white mb-3 ms-0" >
+                            <Card.Img src={newsImage_2} alt="Card image"/>
+                            <Card.ImgOverlay>
+                                <Card.Header>*Время* *Сфера*</Card.Header>
+                                <Card.Text className="align-text-top">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</Card.Text>
+                            </Card.ImgOverlay>
+                        </Card>
+                        <Card className="bg-dark text-white mb-3">
+                            <Card.Img src={newsImage_3} alt="Card image"/>
+                            <Card.ImgOverlay>
+                                <Card.Header>*Время* *Сфера*</Card.Header>
+                                <Card.Text className="align-text-top">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</Card.Text>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <div className = "d-flex justify-content-end mb-3 me-3">
+                <button type="button" className="btn btn-secondary">Перейти к новостям</button>
+            </div>
         </>
     );
 }
@@ -106,13 +178,12 @@ export default function Main() {
             <FirstMainBlock/>
             <div>
                 <Card
-                    bg="primary"
-                    text="white"
-                    style={{ "width": "440px", "height": "56px" }}
-                    className="mb-2 ms-3 w-600">
-                    <h1 className="text-center">Новости</h1>
+                    className="text-center ms-3 bg-secondary text-white mb-3 ms-3 w-600"
+                    style={{ "width": "440px", "height": "56px" }}>
+                    <h1>Новости</h1>
                 </Card>
             </div>
+            <NewsBlock/>
             <ProjectsSlider/>
         </div>
     );
